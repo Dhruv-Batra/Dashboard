6229772c925b2bc5d2860ca8a973c56067a16f67
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-01 14:57:21 -0400

    recursive get all students

--- server/routes/dhruv.js
+++ server/routes/dhruv.js
@@ -12,4 +12,25 @@ router.get('/events', async(req, res) => {
     res.send(allEvents);
 });
 
+router.get('/students', async(req,res)=> {
+    const snapshot = await db.collection('classes').get();
+    const teachIds=[];
+    var studentList = {};
+    var index = 0;
+    snapshot.forEach((teacher) => {
+        teachIds.push(teacher.id);
+    })
+    recurTeach(teachIds,index, studentList,res);
+})
+
+async function recurTeach(teachIds,index,studentList,res){
+    if (index >= teachIds.length)
+        return res.send(studentList);
+    let teachSnapshot = await db.collection('classes').doc(teachIds[index]).collection('students').get();
+    teachSnapshot.forEach((student) => {
+        studentList[student.id]=student.data();
+    })
+    recurTeach(teachIds,index+1, studentList,res);
+}
+
 module.exports = router;
--- src/components/Dhruv.js
+++ src/components/Dhruv.js
@@ -3,16 +3,24 @@ import { useEffect } from "react"
 export default function Dhruv(){
 
     useEffect(() => {
-        fetch('http://localhost:8080/dhruv/events')
-        .then(response => response.json())
-        .then((resp) => {
-            console.log(resp)
-        });
+        // GET all events
+        // fetch('http://localhost:8080/dhruv/events')
+        // .then(response => response.json())
+        // .then((resp) => {
+        //     console.log(resp)
+        // });
+        // GET all student information
+        // fetch('http://localhost:8080/dhruv/')
+        // .then(response => response.json())
+        // .then((resp) => {
+        //     console.log(resp)
+        // });
+
     })
 
     return(
         <div>
-        
+            
         </div>
     )
 }
