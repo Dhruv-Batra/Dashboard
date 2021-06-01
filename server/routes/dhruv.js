bff42aea929a80616e80dfeab50aa67a0c42a718
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-01 10:48:45 -0400

    fill

--- server/routes/dhruv.js
+++ server/routes/dhruv.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const db = require('../firebase')
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    res.sendStatus(200);
 });
 
 module.exports = router;
