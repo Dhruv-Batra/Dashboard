1e262690858dd3ffbe372fba3244c96785bbc9bd
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-01 08:53:31 -0400

    file setup

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+
+import Calendar from './Calendar/Calendar'
+import Classes from './Classes/Classes'
+import Home from './Home/Home'
+import Student from './Student/Student'
+import Teacher from './Teacher/Teacher'
+
+export default function Main(){
+    return(
+        <div>
+            <Switch>
+                <Route path="Teacher"  component={Teacher} />
+                <Route path="Student"  component={Student} />
+                <Route path="Classes"  component={Classes} />
+                <Route path="/calendar"  component={Calendar} />
+                <Route path="/" exact component={Home} />   
+            </Switch>
+        </div>
+    )
+}
