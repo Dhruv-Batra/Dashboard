8266108d1ddc9aeebc58a974a2b94690b14503d4
Nolan <nolan@Nolans-MacBook-Pro.local>
2021-06-02 17:14:51 -0400

    created calendar page

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,13 +1,58 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  withStyles,
+  TextField,
+  InputAdornment,
+  Card,
+  Paper,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function Calendar() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 300,
+      width: 250,
+      backgroundColor: "#FDC700",
+      margin: 40,
+      marginBottom: 20,
+      marginLeft: 67,
+      textAlign: "center",
+      paddingTop: 50,
+      lineHeight: 2,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+  const [event, setEvent] = useState([]);
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
   // function to get events
   // GET all events
+
   const getEvents = () => {
     fetch("http://localhost:8080/events/get")
-      .then((response) => response.json())
-      .then((resp) => {
-        console.log(resp);
+      .then((res) => {
+        return res.json();
+      })
+      .then((obj) => {
+        if (obj != null) {
+          console.log("API CALL", obj);
+          if (obj.length === 0) {
+            obj = [{ volumeInfo: { title: "Book not found.", authors: [""] } }];
+          }
+          setEvent(obj);
+        } else {
+          console.log("Oops.");
+        }
       });
   };
   useEffect(() => {
@@ -16,9 +61,66 @@ export default function Calendar() {
   //function to add events
   // functio to delete events
   // function to update events
+
+  //useStyles
+
+  const StyledCard = withStyles({
+    root: {
+      background: "linear-gradient(45deg, #FDC700 30%, #FDC700 90%)", // gradient color l -> r
+      borderRadius: 3,
+      width: 200,
+      border: 0,
+      color: "black", // text color
+      height: 300,
+      padding: "0 30px",
+      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      display: "flex",
+      flexDirection: "row",
+    },
+    label: {
+      //textTransform: 'capitalize',
+    },
+  })(Card);
+
   return (
     // display events
 
-    <div></div>
+    // <div>
+    //   {event.map((event, index) => (
+    //     <div>
+    //       <StyledCard>{event.title}</StyledCard>
+    //     </div>
+    //   ))}
+    // </div>
+    <div classname="big-boi-div">
+      <div className="card-container">
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="left" spacing={spacing}>
+              {event.map((event, index) => (
+                <Grid key={index} event>
+                  <Paper className={classes.paper}>
+                    <Typography variant="h4">
+                      <Box fontWeight="fontWeightBold" m={1}>
+                        {event.date}
+                      </Box>
+                    </Typography>
+
+                    <br />
+                    <Typography variant="h5">{event.time}</Typography>
+
+                    <br />
+                    <Typography variant="h6">{event.title}</Typography>
+
+                    <br />
+                    <Typography variant="h6">{event.description}</Typography>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,55 @@
-import { Link } from 'react-router-dom';
-import { Button, withStyles } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import { Button, Typography, withStyles } from "@material-ui/core";
 export default function Navbar() {
-    const StyledButton = withStyles({
-        root: {
-          background: 'linear-gradient(45deg, #FDC700 30%, #FDC700 90%)', // gradient color l -> r
-          borderRadius: 3,
-          border: 0,
-          color: 'black', // text color
-          height: 48,
-          padding: '0 30px',
-          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        },
-        label: {
-          //textTransform: 'capitalize',
-        },
-      })(Button);
+  const StyledButton = withStyles({
+    root: {
+      background: "linear-gradient(45deg, #FDC700 30%, #FDC700 90%)", // gradient color l -> r
+      borderRadius: 3,
+      border: 0,
+      color: "black", // text color
+      height: 48,
+      padding: "0 30px",
+      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    },
+    label: {
+      //textTransform: 'capitalize',
+    },
+  })(Button);
 
-    return (
-        <div style={{ padding: "15px", backgroundColor: "#003c6c", display: "flex", flexDirection: "row", justifyContent: "center" }}> 
-        {/* placeholder bar color ^ */}
-            <Link to="/" style={{ textDecoration: "none" }}>
-                <StyledButton style={{ marginLeft: "10px", marginRight: "10px"  }}>
-                    Homepage
-                </StyledButton>
-            </Link>
+  return (
+    <div
+      style={{
+        padding: "15px",
+        backgroundColor: "#003c6c",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      {/* placeholder bar color ^ */}
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
+          Homepage
+        </StyledButton>
+      </Link>
 
-            <Link to="/teacher" style={{ textDecoration: "none" }}>
-                <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
-                    Teacher Directory
-                </StyledButton>
-            </Link>
+      <Link to="/teacher" style={{ textDecoration: "none" }}>
+        <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
+          Teacher Directory
+        </StyledButton>
+      </Link>
 
-            <Link to="/student" style={{ textDecoration: "none" }}>
-            <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
-                    Student Directory
-                </StyledButton>
-            </Link>
+      <Link to="/student" style={{ textDecoration: "none" }}>
+        <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
+          Student Directory
+        </StyledButton>
+      </Link>
 
-            <Link to="/calendar" style={{ textDecoration: "none" }}>
-            <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
-                    Calendar
-                </StyledButton>
-            </Link>
-        </div>
-    );
-};
+      <Link to="/calendar" style={{ textDecoration: "none" }}>
+        <StyledButton style={{ marginLeft: "10px", marginRight: "10px" }}>
+          Calendar
+        </StyledButton>
+      </Link>
+    </div>
+  );
+}
