e70c6d13b748a71d86367e53239c478dbe24a654
A-Gagin <afg4pr@virginia.edu>
2021-06-02 10:20:57 -0400

    Updates for Server and Student Page

--- server/routes/dhruv.js
+++ server/routes/dhruv.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../firebase')
+const axios = require('axios');
 
 router.get('/events', async(req, res) => {
     const snapshot = await db.collection('events').get();
@@ -15,7 +16,7 @@ router.get('/events', async(req, res) => {
 router.get('/students', async(req,res)=> {
     const snapshot = await db.collection('classes').get();
     const teachIds=[];
-    var studentList = {};
+    var studentList = [];
     var index = 0;
     snapshot.forEach((teacher) => {
         teachIds.push(teacher.id);
@@ -28,13 +29,48 @@ async function recurTeach(teachIds,index,studentList,res){
         return res.send(studentList);
     let teachSnapshot = await db.collection('classes').doc(teachIds[index]).collection('students').get();
     teachSnapshot.forEach((student) => {
-        studentList[student.id]=student.data();
+        studentList.push(student.data());
     })
     recurTeach(teachIds,index+1, studentList,res);
 }
 
-router.get('/roster/:teachId', async(req,res) => {
-    console.log(req);
+router.get('/roster', async(req,res) => {
+    let teachSnapshot = await db.collection('classes').doc(req.query.teachId).collection('students').get();
+    var studentList = {};
+    teachSnapshot.forEach((student) => {
+        studentList[student.id]=student.data();
+    })
+    res.send(studentList)
 })
+
+router.delete('/delete-event', async(req,res) => {
+    await db.collection('events').doc(req.query.eventId).delete();
+    res.sendStatus(200);
+})
+
+// router.post('/move', async(req,res) => {
+//     const tarStudent = await db.collection('classes').
+//     console.log(tarStudent);
+//     res.send(tarStudent);
+// })
+
+    // router.post("/move", async (req, res) => {
+    //     const { studentId, currentTeacherId, targetTeacherId } = req.body;
+    //     const targetStudent = await db
+    //      .collection("classes")
+    //      .doc(currentTeacherId)
+    //      .collection("students")
+    //      .doc(studentId)
+    //      .get()
+    //     console.log(targetStudent);
+    //     // const targetTeacher = await db
+    //     // .collection("classes")
+    //     // .doc(targetTeacherId)
+    //     // .collection("students")
+    //     // .set({
+    //     //     targetStudent,
+    //     // });
+    // });
+
 
 module.exports = router;
--- src/components/Dhruv.js
+++ src/components/Dhruv.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import axios from 'axios'
 
 export default function Dhruv(){
 
@@ -18,16 +19,49 @@ export default function Dhruv(){
         // });
         
         //GET student roster per teacher
-        var url=new URL('http://localhost:8080/dhruv/roster'),
-        params=({
-                teachId:'0oG3nsOBuo6UnVYSo4Fc',
-            })
-        url.search = new URLSearchParams(params).toString();
-        fetch(url)
-        .then(response => response.json())
-        .then((resp) => {
-            console.log(resp)
-        });
+        // axios.get('http://localhost:8080/dhruv/roster',{
+        //     params:{
+        //         teachId:'0oG3nsOBuo6UnVYSo4Fc'
+        //     }
+        // })
+        // .then(function (resp){
+        //     console.log(resp.data);
+        // })
+
+        //DELETE event on calendar
+        // axios.delete('http://localhost:8080/dhruv/delete-event',{
+        //     params:{
+        //         eventId: 'DjDgi0rNcnQ4wLTqaHTn',
+        //     }
+        // }).then(function (resp) {
+        //     console.log(resp.data) //should log status message, can delete this
+        // })
+
+        // axios.post('http://localhost:8080/dhruv/move',{
+        //     params:{
+        //         student:'Kuwwog0wBvsshMoDl84a', //change to state
+        //         currentTeacher: 'cOcSMVKGi4KDSQLYZ4wq',
+        //         targetTeacher:'ZFGmQGWbyka60D1Dn91Y', //change to state
+        //     },
+        // })
+        // .then(function (resp){
+        //     console.log(resp.data);
+        // })
+
+        const moveData = {
+            studentId: "ZFGmQGWbyka60D1Dn91Y",
+            currentTeacherId: "0oG3nsOBuo6UnVYSo4Fc",
+            targetTeacherId: "iR8tDX6igwp9632a7XtK",
+          };
+          fetch("http://localhost:8080/dhruv/move", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(moveData),
+          });
+
     })
 
     return(
@@ -37,3 +71,4 @@ export default function Dhruv(){
     )
 
 }
+
